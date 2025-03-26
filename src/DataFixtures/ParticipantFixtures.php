9178f0248f8530486f286204925f1b02025b3798
fandrich2024 <fedeand3@gmail.com>
2025-03-26 16:24:53 +0100

    functional sorties list with participant fixtures

--- src/DataFixtures/ParticipantFixtures.php
+++ src/DataFixtures/ParticipantFixtures.php
@@ -5,7 +5,6 @@ namespace App\DataFixtures;
 use AllowDynamicProperties;
 use App\Entity\Participant;
 use App\Entity\Site;
-use App\Repository\SiteRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -13,21 +12,25 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 #[AllowDynamicProperties] class ParticipantFixtures extends Fixture
 {
 
-
     public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
     }
     public function load(ObjectManager $manager): void
     {
-        $site = $manager->getRepository(Site::class)->find(9);
+        $site1 = new Site();
+        $site1->setNom('SAINT HERBLAIN');
+        $manager->persist($site1);
 
-        if (!$site) {
-            throw new \Exception("El sitio con ID 5 no existe.");
-        }
+        $site2 = new Site();
+        $site2->setNom('CHARTRES DE BRETAGNE');
+        $manager->persist($site2);
 
+        $site3 = new Site();
+        $site3->setNom('LA ROCHE SUR YON');
+        $manager->persist($site3);
 
-
+        // Ahora creamos el 'Participant' y lo asociamos a un 'Site'
         $participant = new Participant();
         $participant->setNom('Doe');
         $participant->setPrenom('John');
@@ -36,20 +39,43 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
         $participant->setAdministrateur(false);
         $participant->setActif(true);
         $participant->setPseudo('johndoe');
-        $participant->setSite($site);
+        $participant->setPassword($this->passwordHasher->hashPassword($participant, '123456'));
 
-        // Encriptar el password
-        $hashedPassword = $this->passwordHasher->hashPassword($participant, '123456');
-        $participant->setPassword($hashedPassword);
-
-        // Establecer roles
-        $participant->setRoles(['ROLE_USER']);
-
-        // Persistir la entidad en la base de datos
+        // Asociamos un Site (en este caso, site1)
+        $participant->setSite($site1);
         $manager->persist($participant);
 
-        // Hacer flush para guardar en la base de datos
-        $manager->flush();
+        $participant2 = new Participant();
+        $participant2->setNom('Smith');
+        $participant2->setPrenom('Emily');
+        $participant2->setTelephone('987654321');
+        $participant2->setMail('emilysmith@example.com');
+        $participant2->setAdministrateur(true);
+        $participant2->setActif(true);
+        $participant2->setPseudo('emilysmith123');
+     $participant2->setSite($site2);
+     $participant2->setPassword($this->passwordHasher->hashPassword($participant2, 'lalala'));
+     $manager->persist($participant2);
 
+
+        // Tercer participante
+        $participant3 = new Participant();
+        $participant3->setNom('Martin');
+        $participant3->setPrenom('Louis');
+        $participant3->setTelephone('555123456');
+        $participant3->setMail('louismartin@example.com');
+        $participant3->setAdministrateur(false);
+        $participant3->setActif(false);
+        $participant3->setPseudo('louis_martin');
+        $participant3->setSite($site3);
+        $participant3->setPassword($this->passwordHasher->hashPassword($participant3, 'tatata'));
+        $manager->persist($participant3);
+
+        // Persistimos el participante
+
+
+
+
+        $manager->flush();
     }
 }

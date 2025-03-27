1c8e61e24d049cb6af85ace54cdb3c7e64e17fc7
Pierre-B <pierre-boujonnier2024@campus-eni.fr>
2025-03-27 11:28:56 +0100

    > Corrections sortie filters

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\ParticipantProfileType;
+use App\Form\SortieFilterType;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Sortie;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -17,11 +19,84 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(SortieRepository $sortieRepository): Response
+    public function index(
+        SortieRepository $sortieRepository,
+        Request $request,
+        Security $security,
+    ): Response
     {
-$sorties=$sortieRepository->findAll();
-        return $this->render('main/index.html.twig',[
-            'sorties'=>$sorties
+        // Appel du fomulaire de filtres
+        $filterForm = $this->createForm(SortieFilterType::class);
+        $filterForm->handleRequest($request);
+
+        // Récupération des critères de filtres
+        $site = $filterForm->get('site')->getData();
+        $nom = $filterForm->get('nom')->getData();
+        $dateDebut = $filterForm->get('dateDebut')->getData();
+        $dateFin = $filterForm->get('dateFin')->getData();
+        $organisateur = $filterForm->get('organisateur')->getData();
+        $inscrit = $filterForm->get('inscrit')->getData();
+        $nonInscrit = $filterForm->get('nonInscrit')->getData();
+        $passee = $filterForm->get('passee')->getData();
+
+        // Création du builder de la requête qui va assembler les différents filtres
+        $queryBuilder = $sortieRepository->createQueryBuilder('s');
+
+        // Sélecteur prposeant les différents sites
+        if ($site) {
+            $queryBuilder->andWhere('s.site = :site')
+                ->setParameter('site', $site);
+        }
+
+        // Barre de recherche par mot
+        if ($nom) {
+            $queryBuilder->andWhere('s.nom LIKE :nom')
+                ->setParameter('nom', '%' . $nom . '%');
+        }
+
+        // INTERVAL
+        //// Sélecteur début de l'interval
+        if ($dateDebut) {
+            $queryBuilder->andWhere('s.dateHeureDebut >= :dateDebut')
+                ->setParameter('dateDebut', $dateDebut);
+        }
+        //// Sélecteur fin de l'interval
+        if ($dateFin) {
+            $queryBuilder->andWhere('s.dateHeureDebut <= :dateFin')
+                ->setParameter('dateFin', $dateFin);
+        }
+
+        // CHECKLIST
+        //// Checkpoint Utilisateur est organisateur
+        if ($organisateur) {
+            $queryBuilder->andWhere('s.organisateur = :organisateur')
+                ->setParameter('organisateur', $security->getUser());
+        }
+
+        //// Checkpoint Utilisateur est inscrit
+        if ($inscrit) {
+            $queryBuilder->andWhere(':user MEMBER OF s.participants')
+                ->setParameter('user', $security->getUser());
+        }
+
+        //// Checkpoint Utilisateur n'est organisateur
+        if ($nonInscrit) {
+            $queryBuilder->andWhere(':user NOT MEMBER OF s.participants')
+                ->setParameter('user', $security->getUser());
+        }
+
+        //// Checkpoint les Sorties sont passées
+        if ($passee) {
+            $queryBuilder->andWhere('s.dateHeureDebut < :now')
+                ->setParameter('now', new \DateTime());
+        }
+
+        // Affichage des sorties correspondant aux critères sélectionnés
+        $sorties = $queryBuilder->getQuery()->getResult();
+
+        return $this->render('main/index.html.twig', [
+            'sorties' => $sorties,
+            'filterForm' => $filterForm,
         ]);
     }
 
@@ -43,10 +118,11 @@ $sorties=$sortieRepository->findAll();
     #[Route('/profil/edit', name: 'app_profil_edit')]
     #[IsGranted('ROLE_USER')]
     public function modifierProfil(
-        Request $request,
-        EntityManagerInterface $entityManager,
+        Request                     $request,
+        EntityManagerInterface      $entityManager,
         UserPasswordHasherInterface $passwordHasher
-    ): Response {
+    ): Response
+    {
         /** @var Participant $participant */
         $participant = $this->getUser();
         $oldPassword = $participant->getPassword(); // Correction: $participant au lieu de $participant

19f592ea611cdaabfe3656a734a32d6807c2c74f
pboujonnier2024 <pierre.boujonnier2024@campus-eni.fr>
2025-04-01 11:49:32 +0200

    formulaire mise en place des lieux adaptés à l'id

--- public/js/villes-lieux-selectors.js
+++ public/js/villes-lieux-selectors.js
@@ -1,6 +1,23 @@
 let villeInput = document.getElementById('sortie_ville');
 let lieuInput = document.getElementById('sortie_lieu');
+lieuInput.innerHTML = '<option value="">Veuillez choisir une ville</option>';
 
 villeInput.addEventListener('change', function () {
-        lieuInput.innerHTML = 'none';
+    lieuInput.innerHTML = '';
+    let option = document.createElement('option');
+    option.value = null;
+    option.textContent = "Veuillez selectionner un lieu";
+    option.disabled = true;
+    option.defaultSelected = true;
+    lieuInput.appendChild(option);
+    fetch('/villes/' + villeInput.value + '/lieux')
+        .then(response => response.json())
+        .then(lieux => {
+            lieux.forEach(lieu => {
+                let option = document.createElement('option');
+                option.value = lieu.id;
+                option.textContent = lieu.nom;
+                lieuInput.appendChild(option);
+            });
+        })
 });
